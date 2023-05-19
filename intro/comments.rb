00dbac05b5fa8faaa38f532a35449a66e549675c
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-19 12:54:50 +0300

    added comments example

---
+++ intro/comments.rb
@@ -0,0 +1,12 @@
+# puts "Here is a comment of some code"
+
+# Here is just a general comment!
+
+# Don't rely on comments as documentation!
+
+=begin T
+his is a comment
+block, which means
+multine line
+comments can exist
+=end
