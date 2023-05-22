085321c0aef31ddb321f16de7b985beea718e699
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-22 11:36:18 +0300

    added hash example

---
+++ intro/hash.rb
@@ -0,0 +1,17 @@
+consoles1 = {
+  "Sega MegaDrive" => "MD",
+  "Super Nintendo" => "SNES",
+  "Sony Playstation" => "PSX"
+}
+
+consoles2 = {
+  :MegaDrive => "MD",
+  :Playstation => "PSX"
+}
+
+
+
+puts consoles1
+puts consoles1["Sony Playstation"]
+
+puts consoles2[:MegaDrive]
